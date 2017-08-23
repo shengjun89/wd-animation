ee3bee97e7eddfbe47998bb00a610aa5e1010acb
刷牙君 <136359818@qq.com>
2017-08-23 17:10:01 +0800

    first upload
    lottie animation

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('loading'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+})
+//https://github.com/bodymovin/bodymovin
